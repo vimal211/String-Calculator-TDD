60eef60c0ffab0c96d0b80b3f9ac8699ef814236
vimal211 <vimalnavee2110@gmail.com>
2025-06-28 12:25:40 +0530

    Red: Add test for negative numbers exception

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -35,4 +35,13 @@ describe("StringCalculator", () => {
     expect(calculator.add("//;\n1;2")).toBe(3);
     expect(calculator.add("//|\n1|2|3")).toBe(6);
   });
+
+  it("should throw exception for negative numbers", () => {
+    expect(() => calculator.add("-1,2")).toThrow(
+      "negative numbers not allowed -1"
+    );
+    expect(() => calculator.add("2,-4,3,-5")).toThrow(
+      "negative numbers not allowed -4,-5"
+    );
+  });
 });
