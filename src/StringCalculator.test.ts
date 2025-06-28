40ae074e4a5d5a9a42f4d9256717e19f2dfc8904
vimal211 <vimalnavee2110@gmail.com>
2025-06-28 12:10:54 +0530

    Red: Add test for single number

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -10,4 +10,9 @@ describe("StringCalculator", () => {
   it("should return 0 for empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  it("should return the number itself for single number", () => {
+    expect(calculator.add("1")).toBe(1);
+    expect(calculator.add("5")).toBe(5);
+  });
 });
