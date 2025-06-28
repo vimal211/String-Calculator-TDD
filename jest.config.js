a1bf76e2c9ade066b76d5dc4e27ef7fc73bb1409
vimal211 <vimalnavee2110@gmail.com>
2025-06-28 12:07:15 +0530

    config changes

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.ts$": "ts-jest",
   },
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
 };
